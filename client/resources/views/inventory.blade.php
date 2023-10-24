efa30e72c69d4937f5b51fa2c331a13da1e078ac
Martin <meatole.cs@gmail.com>
2023-10-24 22:46:48 +0800

    Added header component

---
+++ client/resources/views/inventory.blade.php
@@ -0,0 +1,143 @@
+@extends('layouts.app')
+@section('content')
+<div>
+    <x-header />
+    <div class="px-6">
+        <div class="grid grid-cols-2 gap-4 pb-[100px]">
+                <div class="flex-grow bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div class="mx-auto p-4">
+                        <img class="rounded-t-lg" src="{{ asset('assets/kankong.png') }}" alt="" width="200"
+                            height="150" />
+                    </div>
+                    <div class="p-5">
+                        <a href="#">
+                            <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">KangKong</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Product Description</p>
+                        <a href="#"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            View Product
+                            <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                        <p class="mb-4 font-normal text-sm text-gray-700 dark:text-gray-400">Seller Name</p>
+                        <p class="font-bold text-md">₱ 15.00 / kg</p>
+                    </div>
+                </div>
+                <div class="flex-grow bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div class="mx-auto p-4">
+                        <img class="rounded-t-lg" src="{{ asset('assets/kankong.png') }}" alt="" width="200"
+                            height="150" />
+                    </div>
+                    <div class="p-5">
+                        <a href="#">
+                            <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">KangKong</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Product Description</p>
+                        <a href="#"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            View Product
+                            <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                        <p class="mb-4 font-normal text-sm text-gray-700 dark:text-gray-400">Seller Name</p>
+                        <p class="font-bold text-md">₱ 15.00 / kg</p>
+                    </div>
+                </div>
+                <div class="flex-grow bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div class="mx-auto p-4">
+                        <img class="rounded-t-lg" src="{{ asset('assets/kankong.png') }}" alt="" width="200"
+                            height="150" />
+                    </div>
+                    <div class="p-5">
+                        <a href="#">
+                            <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">KangKong</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Product Description</p>
+                        <a href="#"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            View Product
+                            <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                        <p class="mb-4 font-normal text-sm text-gray-700 dark:text-gray-400">Seller Name</p>
+                        <p class="font-bold text-md">₱ 15.00 / kg</p>
+                    </div>
+                </div>
+                <div class="flex-grow bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div class="mx-auto p-4">
+                        <img class="rounded-t-lg" src="{{ asset('assets/kankong.png') }}" alt="" width="200"
+                            height="150" />
+                    </div>
+                    <div class="p-5">
+                        <a href="#">
+                            <h5 class="mb text-md font-bold tracking-tight text-gray-900 dark:text-white">KangKong</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Product Description</p>
+                        <a href="#"
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            View Product
+                            <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            </svg>
+                        </a>
+                        <p class="mb-4 font-normal text-sm text-gray-700 dark:text-gray-400">Seller Name</p>
+                        <p class="font-bold text-md">₱ 15.00 / kg</p>
+                    </div>
+                </div>
+                <div class="flex-grow bg-white border border-gray-200 rounded-lg shadow">
+                    <div class="mx-auto p-2">
+                        <img class="rounded-t-lg mx-auto" src="{{ asset('assets/kankong.png') }}" alt=""
+                            width="150" height="100" />
+                    </div>
+                    <div class="p-5">
+                        <a href="#">
+                            <h5 class="mb text-md font-bold tracking-tight text-gray-900 dark:text-white">KangKong</h5>
+                        </a>
+                        <p class="mb-4 font-normal text-sm text-gray-700 dark:text-gray-400">Seller Name</p>
+                        <p class="font-bold text-md">₱ 15.00 / kg</p>
+                    </div>
+                </div>
+                <div class="flex-grow bg-white border border-gray-200 rounded-lg shadow">
+                    <div class="mx-auto p-2">
+                        <img class="rounded-t-lg mx-auto" src="{{ asset('assets/kankong.png') }}" alt=""
+                            width="150" height="100" />
+                    </div>
+                    <div class="p-5">
+                        <a href="#">
+                            <h5 class="mb text-md font-bold tracking-tight text-gray-900 dark:text-white">KangKong</h5>
+                        </a>
+                        <p class="mb-4 font-normal text-sm text-gray-700 dark:text-gray-400">Seller Name</p>
+                        <p class="font-bold text-md">₱ 15.00 / kg</p>
+                    </div>
+                </div>
+                <div class="flex-grow bg-white border border-gray-200 rounded-lg shadow">
+                    <div class="mx-auto p-2">
+                        <img class="rounded-t-lg mx-auto" src="{{ asset('assets/kankong.png') }}" alt=""
+                            width="150" height="100" />
+                    </div>
+                    <div class="p-5">
+                        <a href="#">
+                            <h5 class="mb text-md font-bold tracking-tight text-gray-900 dark:text-white">KangKong</h5>
+                        </a>
+                        <p class="mb-4 font-normal text-sm text-gray-700 dark:text-gray-400">Seller Name</p>
+                        <p class="font-bold text-md">₱ 15.00 / kg</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+@endsection
