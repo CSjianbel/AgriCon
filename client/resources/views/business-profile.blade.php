da8f73b1e2dd22217249af56b0493725d7320401
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2023-10-25 07:18:52 +0800

    feat: added logo in sign in form

---
+++ client/resources/views/business-profile.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+    <x-header />
+
+    <div class="px-6 flex items-center justify-center">
+        <div class="flex items-center justify-between border h-[200px] w-full rounded-lg px-10">
+            <div class="flex flex-items">
+                <div class="bg-gray-600 h-[70px] w-[70px] rounded-full">
+                </div>
+                <div class="flex flex-col ml-4">
+                    <p class="text-gray-600  text-2xl font-bold ">Martin the Business</p>
+                    <p class="text-gray-600 text-base font-medium">Martin Edgar Atole</p>
+                    <p class="text-gray-600 text-sm mb-2">Cararayan, Naga City</p>
+                   
+                    <div class="flex-col items-center gap-x-2">
+                        <p class="text-yellow-400">★★★★★</p>
+                        <p class="text-sm text-gray-600">Rating: 5/5</p>
+                    </div>
+               
+                </div>
+                
+            </div>
+            {{-- <form method="GET" action="{{ route('logout') }}">
+                @csrf
+                <button type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                    </svg>  
+                </button>
+            </form> --}}
+        </div>
+    </div>
+
+    <p class="text-gray-600 font-bold mb-4 px-6 mt-6">Offered Products</p>
+
+    <div class="grid grid-cols-2 gap-4 pb-[100px] px-6">
+        @for ($i = 1; $i <= 8; $i++)
+            <x-product-card />
+        @endfor
+    </div>
+
+    <x-bottom-nav />
+@endsection
--- client/resources/views/signin.blade.php
+++ client/resources/views/signin.blade.php
@@ -1,24 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
+
     <div class="px-10 flex items-center justify-center h-screen">
-        <form class="w-full"  action="{{ route('login') }}" method="POST">
-            @csrf
-            <div class="mb-6">
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter email</label>
-                <input type="email" name="email" id="email" class="bg-gray-50 py-4 px-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#1CB87E] focus:border-[#1CB87E] block w-full p-2.5 " placeholder="sample@email.com" required>
-            </div>
-            <div class="mb-4">
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter password</label>
-                <input type="password" name="password" id="password" class="bg-gray-50 py-4 px-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#1CB87E] focus:border-[#1CB87E] block w-full p-2.5 " placeholder="••••••" required>
-            </div>
-            <div class="flex items-start mb-16">
-                <div class="flex items-center h-5">
-                    <input id="remember" type="checkbox" value="" class="accent-pink-500 w-4 h-4 border border-gray-300 rounded focus:ring-3 focus:ring-blue-300">
+        <div class="w-full">
+            <img src="{{ asset('assets/logo.png') }}" alt="" class="mx-auto" width="200">
+            <img src="{{ asset('assets/agricon-transparent-logo.png') }}" alt="" class="mx-auto mb-16" width="130">
+
+            <form class="w-full"  action="{{ route('login') }}" method="POST">
+                @csrf
+                <div class="mb-6">
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter email</label>
+                    <input type="email" name="email" id="email" class="bg-gray-50 py-4 px-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#1CB87E] focus:border-[#1CB87E] block w-full p-2.5 " placeholder="sample@email.com" required>
                 </div>
-                <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-            </div>
-            <button type="submit" class="text-white bg-[#1CB87E] hover:bg-[#168a5f] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-4 text-center">Sign In</button>
-        </form>
+                <div class="mb-4">
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter password</label>
+                    <input type="password" name="password" id="password" class="bg-gray-50 py-4 px-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#1CB87E] focus:border-[#1CB87E] block w-full p-2.5 " placeholder="••••••" required>
+                </div>
+                <div class="flex items-start mb-16">
+                    <div class="flex items-center h-5">
+                        <input id="remember" type="checkbox" value="" class="accent-pink-500 w-4 h-4 border border-gray-300 rounded focus:ring-3 focus:ring-blue-300">
+                    </div>
+                    <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                </div>
+                <button type="submit" class="text-white bg-[#1CB87E] hover:bg-[#168a5f] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-4 text-center mb-6">Sign In</button>
+                <div class="flex flex-row gap-x-2">
+                    <p>Don't have an account?</p>
+                    <a href="{{ route('signup') }}" class="text-[#1CB87E]">Create an account</a>
+                </div>
+            </form>
+        </div>
+
     </div>
 @endsection
