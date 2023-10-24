62b14090a359637f6c1a9697488a39dedcae316d
Albert Perez <albperez@gbox.adnu.edu.ph>
2023-10-25 03:21:02 +0800

    feat: rating

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -1,10 +1,10 @@
-from .models import Post
 from .user import User
 from .order import Order
 from .order_items import OrderItem
 from .item import Item
 from .user import User
 from .inventory import Inventory
+from .rating import Rating
 from ..config import Base, engine
 
 def create_tables():
@@ -13,6 +13,7 @@ def create_tables():
     Base.metadata.create_all(bind=engine, tables=[Order.__table__])
     Base.metadata.create_all(bind=engine, tables=[Inventory.__table__])
     Base.metadata.create_all(bind=engine, tables=[OrderItem.__table__])
-
+    Base.metadata.create_all(bind=engine, tables=[Rating.__table__])
+    
 # Call create_tables when this module is imported
 create_tables()
--- server/app/models/user.py
+++ server/app/models/user.py
@@ -5,6 +5,7 @@ import datetime
 from sqlalchemy.orm import relationship
 from .order import Order
 from .inventory import Inventory
+from .rating import Rating
 
 class User(Base):
     __tablename__ = "users"
@@ -21,3 +22,5 @@ class User(Base):
     farm_orders = relationship('Order', back_populates="farm", foreign_keys=[Order.farm_id])
     items = relationship("Item", back_populates="farm")
     inventory = relationship("Inventory", back_populates="user", foreign_keys=[Inventory.user_id])
+    business_ratings = relationship('Rating', back_populates="business", foreign_keys=[Rating.business_id])
+    farm_ratings = relationship('Rating', back_populates="farm", foreign_keys=[Rating.farm_id])
