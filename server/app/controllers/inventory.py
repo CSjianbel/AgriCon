1cc8c2a95d6af41e360357c2d0751ee1a0f02483
Albert Perez <albperez@gbox.adnu.edu.ph>
2023-10-25 00:13:51 +0800

    feat: inventory crud

--- server/app/controllers/inventory.py
+++ server/app/controllers/inventory.py
@@ -32,45 +32,19 @@ def get_inventories(db: Session, filters: dict, skip: int = 0, limit: int = 100)
 
     return query.offset(skip).limit(limit).all()
 
-# def create_item(db: Session, item: dict, farm_id: int):
-#     db_item = Item(
-#         farm_id=farm_id,
-#         name=item['name'],
-#         unit_of_measure=item['unit_of_measure'],
-#         category=item['category'],
-#         status=item['status'],
-#         )
-#     db.add(db_item)
-#     db.commit()
-#     db.refresh(db_item)
-#     return db_item
+def get_inventory(db: Session, inventory_id: int):
+    return db.execute(select(Inventory).where(Inventory.id == inventory_id)).scalar_one()
 
-# def get_item(db: Session, item_id: int):
-#     return db.execute(select(Item).where(Item.id == item_id)).scalar_one()
+def update_inventory(db: Session, inventory_id: int, inventory: dict):
+    db_inventory = get_inventory(db, inventory_id=inventory_id)
+    for key, value in inventory.items():
+        setattr(db_inventory, key, value)
+    db.commit()
+    db.refresh(db_inventory)
+    return db_inventory
 
-# def get_items(db: Session, filters: dict, skip: int = 0, limit: int = 100):
-#     query = db.query(Item)
-#     if 'item_id' in filters:
-#         query = query.filter(Item.id == filters['item_id'])
-#     if 'farm_id' in filters:
-#         query = query.filter(Item.farm_id == filters['farm_id'])
-#     if 'category' in filters:
-#         query = query.filter(Item.category == filters['category'])
-#     if 'status' in filters:
-#         query = query.filter(Item.status == filters['status'])
-
-#     return query.offset(skip).limit(limit).all()
-
-# def update_item(db: Session, item_id: int, item: dict):
-#     db_item = get_item(db, item_id=item_id)
-#     for key, value in item.items():
-#         setattr(db_item, key, value)
-#     db.commit()
-#     db.refresh(db_item)
-#     return db_item
-
-# def delete_item(db: Session, item_id: int):
-#     db_item = get_item(db, item_id=item_id)
-#     db.delete(db_item)
-#     db.commit()
-#     return db_item
+def delete_inventory(db: Session, inventory_id: int):
+    db_inventory = get_inventory(db, inventory_id=inventory_id)
+    db.delete(db_inventory)
+    db.commit()
+    return db_inventory
--- server/app/routers/inventory.py
+++ server/app/routers/inventory.py
@@ -4,7 +4,7 @@ from typing import Union
 
 
 from ..config import SessionLocal, get_db
-from ..controllers import create_inventory, get_inventories
+from ..controllers import create_inventory, get_inventories, update_inventory, delete_inventory
 
 # Define your APIRouter
 inventory_router = APIRouter()
@@ -40,40 +40,12 @@ def read_inventories(
     inventories = get_inventories(db, filters=filters, skip=skip, limit=limit)
     return inventories
 
-# @item_router.post("/create")
-# def create(item: dict, farm_id: int, db: Session = Depends(get_db)):
-#     db_item = create_item(db=db, item=item, farm_id=farm_id)
-#     return db_item
+@inventory_router.patch("/update/{inventory_id}")
+def update(inventory_id, inventory: dict, db: Session = Depends(get_db)):
+    db_inventory = update_inventory(db, inventory_id=inventory_id, inventory=inventory)
+    return db_inventory
 
-# @item_router.get("/")
-# def read_items(
-#     item_id: Union[int, None] = Query(None, description="Filter by item ID"),
-#     farm_id: Union[int, None] = Query(None, description="Filter by farm ID"),
-#     category: Union[str, None] = Query(None, description="Filter by category"),
-#     status: Union[str, None] = Query(None, description="Filter by status"),
-#     skip: int = Query(0, description="Skip the first N items"),
-#     limit: int = Query(100, description="Limit the number of items to retrieve"),
-#     db: Session = Depends(get_db)
-# ):
-#     filters = {}
-#     if item_id is not None:
-#         filters['item_id'] = item_id
-#     if farm_id is not None:
-#         filters['farm_id'] = farm_id
-#     if category is not None:
-#         filters['category'] = category
-#     if status is not None:
-#         filters['status'] = status
-
-#     items = get_items(db, filters=filters, skip=skip, limit=limit)
-#     return items
-
-# @item_router.patch("/update/{item_id}")
-# def patch_item(item_id, item: dict, db: Session = Depends(get_db)):
-#     db_item = update_item(db, item_id=item_id, item=item)
-#     return db_item
-
-# @item_router.delete("/delete/{item_id}")
-# def remove_item(item_id: int, db: Session=Depends(get_db)):
-#     delete_item(db, item_id)
-#     return {"message": "deleted item!"}
+@inventory_router.delete("/delete/{inventory_id}")
+def remove(inventory_id: int, db: Session=Depends(get_db)):
+    delete_inventory(db, inventory_id)
+    return {"message": "deleted inventory!"}
