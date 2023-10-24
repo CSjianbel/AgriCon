64013d07f031629ef6fec0fb0cd10e8e620ab9c1
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2023-10-25 06:06:25 +0800

    feat: added logout functionality

--- client/resources/views/farmer-profile.blade.php
+++ client/resources/views/farmer-profile.blade.php
@@ -13,10 +13,13 @@
                     <p class="text-gray-600 text-sm ml-4">Cararayan, Naga City</p>
                 </div>
             </div>
-            <form method="GET" route="{{ route('logout') }}">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
-                </svg>                  
+            <form method="GET" action="{{ route('logout') }}">
+                @csrf
+                <button type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                    </svg>  
+                </button>
             </form>
         </div>
     </div>
