a5fe1fc660030b98dee72995f89104d34fc3cce9
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2023-10-25 07:49:19 +0800

    feat: add logout button for both users

--- client/resources/views/farmer-profile.blade.php
+++ client/resources/views/farmer-profile.blade.php
@@ -22,14 +22,18 @@
                 </div>
                 
             </div>
-            {{-- <form method="GET" action="{{ route('logout') }}">
-                @csrf
-                <button type="submit">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
-                    </svg>  
-                </button>
-            </form> --}}
+
+            @if (session('user_type') == 'Farmer')
+                <form method="GET" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                        </svg>  
+                    </button>
+                </form>
+            @endif
+
         </div>
     </div>
 
