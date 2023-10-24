f4a0a864c9ab3e18599a7168bac3127c07e9ea63
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2023-10-24 14:28:29 +0800

    feat: added app layout

--- client/resources/views/layouts/app.blade.php
+++ client/resources/views/layouts/app.blade.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
     @vite('resources/css/app.css')
 </head>
 <body>
-    <h1 class="text-red-500">Hello</h1>
+    <main class="py-4">
+        @yield('content')
+    </main>
 </body>
 </html>
