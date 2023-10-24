6ecc82457c67d732a23295fea911079a9eb42751
CSjianbel <jiankarlo99@gmail.com>
2023-10-25 03:44:15 +0800

    feat: changed ports

--- server/main.py
+++ server/main.py
@@ -18,5 +18,5 @@ def home():
     return {"message": "Welcome to AgriCon's API."}
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
 
