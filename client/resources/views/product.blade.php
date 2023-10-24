ef958c5f2e0ee1580d4a83bf27faae36fa898942
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2023-10-25 06:22:01 +0800

    feat: added back button functionality

--- client/resources/views/product.blade.php
+++ client/resources/views/product.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="w-full">
     <div class="px-6 flex bg-white items-center justify-between h-[80px]">
-        <div class="bg-[#edf5ef] p-3 rounded-lg">
+        <button class="bg-[#edf5ef] p-3 rounded-lg" onclick="goBack()">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
             </svg>
-        </div>
+        </button>
         <p class="font-bold">KangKong</p>
         <div class="bg-[#ffffff] text-white p-3 rounded-lg">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
@@ -118,4 +118,10 @@
         </a>
     </div>
 </div>
+
+<script>
+    function goBack() {
+      window.history.back();
+    }
+</script>
 @endsection
--- client/resources/views/transaction_view.blade.php
+++ client/resources/views/transaction_view.blade.php
@@ -2,12 +2,12 @@
 @section('content')
     <div>
         <div class="px-6 flex bg-white items-center justify-between h-[80px]">
-            <div class="bg-[#edf5ef] p-3 rounded-lg">
+            <button class="bg-[#edf5ef] p-3 rounded-lg" onclick="goBack()">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-5 h-5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
                 </svg>
-            </div>
+            </button>
             <p class="font-bold">Transaction Process</p>
             <div class="bg-[#ffffff] text-white p-3 rounded-lg">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -263,5 +263,11 @@
                 });
             });
         });
+
+
+        function goBack() {
+            window.history.back();
+        }
+
     </script>
 @endsection
