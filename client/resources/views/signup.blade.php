ed03107539f63eb37d15db94e4c56d9c316ba0de
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2023-10-24 18:56:03 +0800

    fix: button widths

--- client/resources/views/signup.blade.php
+++ client/resources/views/signup.blade.php
@@ -21,7 +21,7 @@
                 <input type="text" id="disabled-input" placeholder="Create Password" aria-label="disabled input" class="mb-6 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
             </div>
             <hr class="mb-6">
-            <button type="submit" class="text-white mb-6 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-[500px] sm:w-auto px-5 py-2.5 text-center">Create Account</button>
+            <button type="submit" class="text-white mb-6 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center">Create Account</button>
             <div class="flex flex-row gap-x-2">
                 <p>Alread have an account?</p>
                 <a href="#">Sign In</a>
