bb1de823ed28c36804deda6a01674e8aa9a2b3b3
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2023-10-25 06:09:44 +0800

    fix: remove price label in product view

--- client/resources/views/product.blade.php
+++ client/resources/views/product.blade.php
@@ -20,11 +20,10 @@
         <img class="rounded-t-lg mx-auto" src="{{ asset('assets/kankong.png') }}" alt=""
                 width="300" height="300" />
 
-        <div class="flex justify-between mb-6">
-            <div class="flex-col">
-                <p class="font-bold text-2xl">Kangkong</p>
-                <p class="font-bold text-base text-[#1CB87E]">₱ 15.00 / kg</p>
-            </div>
+        <div class="flex justify-between items-center mb-6">
+
+            <p class="font-bold text-2xl">Kangkong</p>
+            
             <button class="p-4 w-fit h-fit bg-[#edf5ef] rounded-lg">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
