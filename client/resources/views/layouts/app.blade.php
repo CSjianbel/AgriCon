437c36dfa8b7e9dfa1e67a1000cfbf592e19cdf5
NoelEmaas <nemaas@gbox.adnu.edu.ph>
2023-10-24 19:42:08 +0800

    fix: auth pages ui

--- client/resources/views/layouts/app.blade.php
+++ client/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body class="bg-slate-100">
     <div class="w-[500px] h-screen bg-white m-auto p-0">
--- client/resources/views/signup.blade.php
+++ client/resources/views/signup.blade.php
@@ -4,24 +4,24 @@
     <div class="px-10 flex items-center justify-center h-screen">
         <form class="w-full">
             <div class="mb-6">
-                <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                <select id="countries" class="bg-gray-50 border py-4 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#1CB87E] focus:border-[#1CB87E] block w-full p-2.5">
                     <option selected>Select User Type</option>
                     <option value="US">Farmer</option>
                     <option value="CA">Business</option>
                 </select>
             </div>
-            <div class="flex flex-row gap-x-4">
-                <input type="text" id="disabled-input" placeholder="First Name" aria-label="disabled input" class="mb-6 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-                <input type="text" id="disabled-input" placeholder="Last Name" aria-label="disabled input" class="mb-6 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <div class="flex flex-row gap-x-4 mb-6">
+                <input type="text" id="first-name" class="bg-gray-50 py-4 px-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#1CB87E] focus:border-[#1CB87E] block w-full p-2.5 " placeholder="First name" required>
+                <input type="text" id="last-name" class="bg-gray-50 py-4 px-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#1CB87E] focus:border-[#1CB87E] block w-full p-2.5 " placeholder="Last name" required>
             </div>
             <div class="mb-6">
-                <input type="text" id="disabled-input" placeholder="Business Name" aria-label="disabled input" class="mb-6 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                <input type="email" id="email" class="bg-gray-50 py-4 px-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#1CB87E] focus:border-[#1CB87E] block w-full p-2.5 " placeholder="Enter email" required>
             </div>
             <div class="mb-6">
-                <input type="text" id="disabled-input" placeholder="Create Password" aria-label="disabled input" class="mb-6 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                <input type="password" id="password" class="bg-gray-50 py-4 px-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#1CB87E] focus:border-[#1CB87E] block w-full p-2.5 " placeholder="Create password" required>
             </div>
             <hr class="mb-6">
-            <button type="submit" class="text-white mb-6 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center">Create Account</button>
+            <button type="submit" class="text-white mb-6 bg-[#1CB87E] hover:bg-[#168a5f] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-4 text-center">Create Account</button>
             <div class="flex flex-row gap-x-2">
                 <p>Alread have an account?</p>
                 <a href="#">Sign In</a>
